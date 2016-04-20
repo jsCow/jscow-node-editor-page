417ed690f66162e5352c218014a966785d6f3709
Christian Todt <christian@chickenfarm.tv>
2016-04-20 13:56:38 +0200

    fixed carousel

--- demo-page/client/views/components/carousel.js
+++ demo-page/client/views/components/carousel.js
@@ -1,32 +1,26 @@
 if (Meteor.isClient) {
 
-    Template.carousel.helpers({
-        
-        carouselID: 'homeCarousel',
-        
-        items: [
-            {
-                title: 'jsCow Node Editor', 
-                subtitle: 'An application for an independend process editor.', 
-                route: '/',
-                imgsrc: '/img/slides/1.jpg',
-                status: 'active'
-            },
-            {
-                title: 'Nodes and Connections', 
-                subtitle: 'A node represents a step of a process.', 
-                route: '/',
-                imgsrc: '/img/slides/2.jpg',
-                status: ''
-            }
-        ]
-        
-    });
-
+  Template.carousel.helpers({
+    carouselID: 'carousel',
+    items: [
+      {
+          title: 'jsCow Node Editor',
+          subtitle: 'An application for an independend process editor.',
+          route: '/',
+          imgsrc: '/img/slides/1.jpg'
+      },
+      {
+          title: 'Nodes and Connections',
+          subtitle: 'A node represents a step of a process.',
+          route: '/',
+          imgsrc: '/img/slides/2.jpg'
+      }
+    ]
+  });
 }
 
 if (Meteor.isServer) {
-    Meteor.startup(function () {
-        // code to run on server at startup
-    });
+  Meteor.startup(function () {
+      // code to run on server at startup
+  });
 }
