9366c581f1bb8b52e61c24d579f4ca5ed04b3b2c
Mario Linz <Mario Linz>
2016-04-20 07:18:52 +0200

    Update...

--- demo-page/client/views/components/carousel.js
+++ demo-page/client/views/components/carousel.js
@@ -4,37 +4,22 @@ if (Meteor.isClient) {
         
         carouselID: 'homeCarousel',
         
-        items: function () {
-            return [
-                {
-                    title: 'jsCow Node Editor', 
-                    subtitle: 'An application for an independend process editor.', 
-                    route: '/',
-                    imgsrc: '/img/slides/1.jpg',
-                    attributes: { 
-                        class: 'item active' 
-                    },
-                    indicatorAttributes: { 
-                        'class': 'active',
-                        'data-target': carouselID, 
-                        'data-slide-to': 1
-                    }
-                },
-                {
-                    title: 'Nodes and Connections', 
-                    subtitle: 'A node represents a step of a process.', 
-                    route: '/',
-                    imgsrc: '/img/slides/2.jpg',
-                    attributes: { 
-                        class: 'item' 
-                    },
-                    indicatorAttributes: {
-                        'data-target': carouselID, 
-                        'data-slide-to': 2
-                    }
-                }
-            ];
-        }
+        items: [
+            {
+                title: 'jsCow Node Editor', 
+                subtitle: 'An application for an independend process editor.', 
+                route: '/',
+                imgsrc: '/img/slides/1.jpg',
+                status: 'active'
+            },
+            {
+                title: 'Nodes and Connections', 
+                subtitle: 'A node represents a step of a process.', 
+                route: '/',
+                imgsrc: '/img/slides/2.jpg',
+                status: ''
+            }
+        ]
         
     });
 
