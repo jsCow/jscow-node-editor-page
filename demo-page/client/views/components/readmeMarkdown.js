17915d8fdd536abc6b6c7e3514f500549514c593
Christian Todt <christian@chickenfarm.tv>
2016-04-29 13:33:02 +0200

    update

--- demo-page/client/views/components/readmeMarkdown.js
+++ demo-page/client/views/components/readmeMarkdown.js
@@ -3,6 +3,7 @@ if (Meteor.isClient) {
     Template.readmeMarkdown.created = function() {
       var url =  'http://raw.githubusercontent.com/jsCow/jscow-node-editor/master/README.md';
       Meteor.call('getRemoteContent', url, function (error, result) {
+        result = result.replace('/src/img', 'http://raw.githubusercontent.com/jsCow/jscow-node-editor/master/src/img');
         Session.set('markdownReadme', result);
       });
     },
